73ba5d4a9fd5547bf9ee26cbc17543a8527b2b76
Earthend <jeanluc.williams@protonmail.com>
2021-09-18 22:51:13 -0700

    Cleaned up solution code to Task 1

--- Task 1/list-ips.sh
+++ Task 1/list-ips.sh
@@ -1,16 +1,21 @@
 #! /bin/bash
+# Must be ran within the directory since I didn't set up the directory reference
 
 # Gather the IPs from the packet into a list
-ips=$(tshark -r capture.pcap  -T fields -e ip.dst | sort | uniq | tr '\n' ' ')
+ips=$(tshark -r ./capture.pcap  -T fields -e ip.dst ip.src | sort | uniq | tr '\n' ' ')
 
-if [ -e 'results.txt' ]; then
-	truncate -s 0 results.txt
-else
-	touch 'results.txt'
+# Run remaining code if we successfully ran the previous content
+if [ $? -eq 0 ]; then
+# Create or reset the results file to copy content to
+	if [ -e 'results.txt' ]; then
+		truncate -s 0 results.txt
+	else
+		touch 'results.txt'
+	fi
+
+	# For each IP in the list, check if they are in one of the ranges
+	for IP in $ips; do
+		grepcidr -f ip_ranges.txt <(echo "$IP") >/dev/null && \
+			echo "$IP is in range" && echo "$IP" >> results.txt
+	done
 fi
-
-# For each IP in the list, check if they are in one of the ranges
-for IP in $ips; do
-	grepcidr -f ip_ranges.txt <(echo "$IP") >/dev/null && \
-		echo "$IP is in range" && echo "$IP" >> results.txt
-done
